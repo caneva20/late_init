e22a325b4b005066ba4ee4a2356d712e0e26aacd
Fabricio Caneva <hardlymersk@gmail.com>
2020-01-28 18:26:40 -0300

    add example

---
+++ example/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:late_init/late_init.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Late Init - Example',
+      home: new HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  HomeScreenState createState() => new HomeScreenState();
+}
+
+class HomeScreenState extends State<HomeScreen> with LateInitMixin {
+  @override
+  void lateInitState() {
+    final someId = ModalRoute.of(context).settings.arguments as String;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Container(color: Colors.red),
+    );
+  }
+}
